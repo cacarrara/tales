d526cf868d72e3a505274aff459250782990d61b
Caio Carrara <eu@caiocarrara.com.br>
2017-07-01 23:38:27 -0300

    Adds poc for view by accept header

--- tests/functional/test_views.py
+++ tests/functional/test_views.py
@@ -1,3 +1,12 @@
-def test_home(testapp):
-    response = testapp.get('/', status=200)
+def test_home_html(testapp):
+    content_type = 'text/html'
+    response = testapp.get('/', headers={'Accept': content_type})
+    assert content_type == response.content_type
     assert b'<h1>Hello' in response.body
+
+
+def test_home_json(testapp):
+    content_type = 'application/json'
+    response = testapp.get('/', headers={'Accept': content_type})
+    assert content_type == response.content_type
+    assert b'{"name":' in response.body
